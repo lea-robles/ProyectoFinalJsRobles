64f6a19e9ecb1ad4d264eb38f3e0f5972a24e479
LeaRobles_75 <learobles75@gmail.com>
2023-07-26 20:24:24 -0300

    otro intento con la url

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ function programaPrincipal() {
     url = "https://raw.githubusercontent.com/lea-robles/ProyectoFinalJsRobles/main/productos.json"
     }
 
-    fetch(url)
+    fetch("https://raw.githubusercontent.com/lea-robles/ProyectoFinalJsRobles/main/productos.json")
         .then(response => response.json())
         .then(data => {
             productos = data.productos
